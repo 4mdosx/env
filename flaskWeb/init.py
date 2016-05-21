c1f95a9044d7f3fe69447f9a6294dcd00c2c05a4
warcryDoggie <warcryDoggie@gmail.com>
2016-05-21 18:38:50 +0800

    reset structure

--- flaskWeb/init.py
+++ flaskWeb/init.py
@@ -4,9 +4,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return '<h1>Hello World</h1>'
+
 @app.route('/user/<name>')
 def user(name):
     return '<h1>Hello,%s!</h1>'%name
+
+@app.errorhandler(404)
+def page_noe_found(e):
+    return
+
+@app.errorhandler(500)
+def 
 
 if __name__ == '__main__':
     app.run(debug=True)
