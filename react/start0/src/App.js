e97f0c8272677c2428ae6999601c9a830370d190
warcryDoggie <warcryDoggie@gmail.com>
2017-07-07 14:26:17 +0800

    save

---
+++ react/start0/src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import CommentBox from './components/commentBox'
+
+var data = [
+  {author: 'Pete Hunt', text: 'This is one comment'},
+  {author: 'Jordan Walke', text: 'This is *another* comment'}
+]
+
+class App extends Component {
+  render () {
+    return (
+      <div className='App'>
+        <div className='App-header'>
+          <img src={logo} className='App-logo' alt='logo' />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className='App-intro'>
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <CommentBox data={data} />
+      </div>
+    )
+  }
+}
+
+export default App
