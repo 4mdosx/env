b7ded5f09b9e5a2efa17d069e0df17208e387687
syntachaosx <chaosdog.tl@gmail.com>
2022-02-14 16:06:08 +0800

    Node: add fastify

---
+++ Node/fastify.mjs
@@ -0,0 +1,16 @@
+import Fastify from 'fastify'
+
+const fastify = Fastify({
+  logger: true,
+})
+
+// Declare a route
+fastify.get('/', (request, reply) => {
+  reply.send({ hello: 'world' })
+})
+
+// Run the server!
+fastify.listen(3000, (err, address) => {
+  if (err) throw err
+  // Server is now listening on ${address}
+})
